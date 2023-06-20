af1c458dcae80dad145c2de30428066107deb2f5
VLuca997 <kulertoast997@gmail.com>
2023-06-20 17:00:46 +0200

    Start JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 // creo un array per i prodotti della spesa
 //stampiamo a schermo tutti i prodotti inseriti nell'array
 
+// array spesa
+const shoppingList = [
+    "Pane", 
+    "Latte",
+    "Uova",
+    "Caffè",
+    "Banane"
+];
 
+//creiamo delle ul per inserirgli la lista della spesa stampata.
+let printList = document.createElement(`ul`);
+
+//inilizializo il contatore
+let indexShopping = 0;
+
+//usiamo un Ciclo while per stampare sul DOM
+while (indexShopping < shoppingList.length) {
+
+    //inizialiizzo le " li " stampate per appendere gli elementi
+    let singleLi = document.createElement(`li`);
+
+    //impostiamo l'elemento attribuendolo alla li designata.
+    
+
+
+
+
+
+
+
+
+}
 
 
 
